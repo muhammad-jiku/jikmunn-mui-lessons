200b644d73254af0c2ac6e6bdefcacbd63ded294
muhammad-jiku <muhammadjiku364@gmail.com>
2023-03-19 14:20:08 +0600

    Rightbar and skeleton

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import { Box, Stack } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Skeleton, Stack } from '@mui/material';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import Feed from './components/Feed';
 import Rightbar from './components/Rightbar';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  setTimeout(() => {
+    setLoading(false);
+  }, [6000]);
+
   return (
     <Box bgcolor={'background.default'} color={'text.primary'}>
       <Navbar />
-      <Stack direction="row" spacing={2} justifyContent="space-between">
-        <Sidebar />
-        <Feed />
-        <Rightbar />
-      </Stack>
+      {loading ? (
+        <Stack spacing={1}>
+          <Skeleton variant="text" height={100} />
+          <Skeleton variant="text" height={20} />
+          <Skeleton variant="text" height={20} />
+          <Skeleton variant="rectangular" height={300} />
+        </Stack>
+      ) : (
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar />
+          <Feed />
+          <Rightbar />
+        </Stack>
+      )}
     </Box>
   );
 }
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,13 +1,8 @@
-import { Box, Skeleton, Stack } from '@mui/material';
-import React, { useState } from 'react';
+import { Box } from '@mui/material';
+import React from 'react';
 import Post from './Post';
 
 const Feed = () => {
-  const [loading, setLoading] = useState(true);
-
-  setTimeout(() => {
-    setLoading(false);
-  }, [3000]);
   return (
     <Box
       flex={4}
@@ -16,20 +11,10 @@ const Feed = () => {
         sm: 2,
       }}
     >
-      {loading ? (
-        <Stack spacing={1}>
-          <Skeleton variant="text" height={100} />
-          <Skeleton variant="text" height={20} />
-          <Skeleton variant="text" height={20} />
-          <Skeleton variant="rectangular" height={300} />
-        </Stack>
-      ) : (
-        <>
-          <Post />
-          <Post />
-          <Post />
-        </>
-      )}
+      <Post />
+      <Post />
+      <Post />
+      <Post />
     </Box>
   );
 };
